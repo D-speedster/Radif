9a8a56e0dd9c675a1b28b51e2bdd3983619767b0
D-speedster <mrspeed717@gmail.com>
2025-05-06 16:41:20 +0330

    create how-to-use page

---
+++ pages/how-to-use/index.js
@@ -0,0 +1,18 @@
+import Header_Main from '../../components/main/header';
+
+import FooterMain from "../../components/main/FooterMain"
+
+export default function HowToUse() {
+    return (
+        <>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="/css/index.css" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+            <Header_Main></Header_Main>
+            <div className="container-custom">
+          
+            </div>
+            <FooterMain></FooterMain>
+        </>
+    )
+}
