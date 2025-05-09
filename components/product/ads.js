8d42c22c529e612f829b883b28d2272a1531d8c9
D-speedster <mrspeed717@gmail.com>
2025-05-09 12:12:47 +0330

    complate Review and Ads component

---
+++ components/product/ads.js
@@ -0,0 +1,12 @@
+export default function AdsComponent() {
+    return (
+        <aside class="col-lg-3 position-relative"
+            style={{ marginTop: '72.5px' }}
+        >
+            <div class="ads">
+                <img class="img-fluid rounded-2" src="/img/ads3.png" alt="" />
+
+            </div>
+        </aside>
+    )
+}
--- pages/product/[slug]/index.js
+++ pages/product/[slug]/index.js
@@ -8,6 +8,8 @@ import SliderProduct from '../../../components/main/SliderProduct'
 import SellersComponent from '../../../components/product/sellers'
 import FeaturesComponent from '../../../components/product/features'
 import PriceHistory from '../../../components/product/PriceHistory'
+import ReviewsComponent from '../../../components/product/Reviews'
+import AdsComponent from '../../../components/product/ads'
 
 export default function ProductPage() {
 
@@ -121,216 +123,17 @@ export default function ProductPage() {
 
                             </section>
                         </div>
-                        <aside class="col-lg-3 position-relative">
-
-                            <img class="img-fluid rounded-2" src="/img/ads2.png" alt="" />
-
-                        </aside>
+                        <AdsComponent />
                     </div >
                 </div >
             </div>
             <div class="container-custom">
                 <div className='row'>
                     <div className='col-lg-9'>
-                        <section id="section2" class="Reviews py-5">
-                            <div class="container-custom">
-                                <h6 class="mt-4">تست ها و بررسی های تخصصی</h6>
-                                <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <ReviewsComponent
+                        />
 
-
-                                    <div class="col">
-                                        <div
-                                            class="top-comment-item p-3 rounded-4 shadow-sm bg-white h-100 border border-light-subtle">
-                                            <div class="d-flex justify-content-between align-items-center mb-2">
-                                                <div class="d-flex align-items-center gap-2">
-                                                    <box-icon type="solid" name="user" size="sm" color="#6c757d"></box-icon>
-                                                    <strong class="text-dark">نقد دیجی کالا</strong>
-                                                </div>
-                                                <small class="text-muted">۱۴۰۳/۰۲/۰۵</small>
-                                            </div>
-                                            <p class="text-secondary small">
-                                                دوربین‌های با کیفیت iPhone 16، تجربه عکاسی فوق‌العاده‌ای را ارائه می‌دهند و
-                                                با فناوری‌های نوین، امکان ثبت لحظات خاص با جزئیات بالا را فراهم می‌کنند
-                                                <span
-                                                    style={{ color: 'blue' }}
-                                                >ادامه ...</span>
-                                            </p>
-                                            <div class="d-flex gap-1 mt-2">
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star-half' type='solid' color='#f9d635'
-                                                    size="sm"></box-icon>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="col">
-                                        <div
-                                            class="top-comment-item p-3 rounded-4 shadow-sm bg-white h-100 border border-light-subtle">
-                                            <div class="d-flex justify-content-between align-items-center mb-2">
-                                                <div class="d-flex align-items-center gap-2">
-                                                    <box-icon type="solid" name="user" size="sm" color="#6c757d"></box-icon>
-                                                    <strong class="text-dark">کاربر نرگس</strong>
-                                                </div>
-                                                <small class="text-muted">۱۴۰۳/۰۲/۰۴</small>
-                                            </div>
-                                            <p class="text-secondary small">طراحی بسیار زیباست ولی شارژدهی می‌تونست بهتر
-                                                باشه.</p>
-                                            <div class="d-flex gap-1 mt-2">
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#e4e4e4' size="sm"></box-icon>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col">
-                                        <div
-                                            class="top-comment-item p-3 rounded-4 shadow-sm bg-white h-100 border border-light-subtle">
-                                            <div class="d-flex justify-content-between align-items-center mb-2">
-                                                <div class="d-flex align-items-center gap-2">
-                                                    <box-icon type="solid" name="user" size="sm" color="#6c757d"></box-icon>
-                                                    <strong class="text-dark">کاربر امین</strong>
-                                                </div>
-                                                <small class="text-muted">۱۴۰۳/۰۲/۰۳</small>
-                                            </div>
-                                            <p class="text-secondary small">قیمتش بالاست ولی کیفیت خوبی داره. پیشنهاد
-                                                می‌کنم.</p>
-                                            <div class="d-flex gap-1 mt-2">
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#f9d635' size="sm"></box-icon>
-                                                <box-icon name='star-half' type='solid' color='#f9d635'
-                                                    size="sm"></box-icon>
-                                                <box-icon name='star' type='solid' color='#e4e4e4' size="sm"></box-icon>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                                <br />
-                                <h3>نظرات : </h3>
-                                <h5>قبل از خرید نگاهی به نظرات دیگران بیندازید.</h5>
-                                <br />
-                                <div class="row align-items-start">
-
-                                    <div class="col-lg-6 mb-4">
-                                        <div>
-                                            <div class="d-flex align-items-center mb-2">
-                                                <span class="fs-1 fw-bold ms-2">4.6</span>
-                                                <span class="text-warning fs-4">
-                                                    <i class="fas fa-star"></i>
-                                                </span>
-                                                <span class="text-muted ms-2">از ۲۳۵ رأی</span>
-                                            </div>
-
-                                            <div
-                                                style={{ maxWidth: '350px' }}
-                                                class="mb-2"
-                                            >
-                                                <div class="d-flex align-items-center mb-1">
-                                                    <span class="ms-2"
-                                                        style={{ width: '24px' }}
-
-                                                    >۵</span>
-                                                    <i class="fas fa-star text-warning ms-2"></i>
-                                                    <div class="progress flex-grow-1 ms-2"
-
-                                                        style={{ height: '6px', backgroundColor: '#f2f2f2' }}
-                                                    >
-                                                        <div class="progress-bar bg-warning" style={{ width: '70%' }}></div>
-                                                    </div>
-                                                    <span class="text-muted" style={{ width: '36px' }}>70%</span>
-                                                </div>
-                                                <div class="d-flex align-items-center mb-1">
-                                                    <span class="ms-2" style={{ width: '24px' }}>۴</span>
-                                                    <i class="fas fa-star text-warning ms-2"></i>
-                                                    <div class="progress flex-grow-1 ms-2" style={{ height: '6px', background: '#f2f2f2' }}
-                                                    >
-                                                        <div class="progress-bar bg-warning"
-                                                            style={{ width: '15%' }}
-                                                        ></div>
-                                                    </div>
-                                                    <span class="text-muted" style={{ width: '36px' }}>15%</span>
-                                                </div>
-                                                <div class="d-flex align-items-center mb-1">
-                                                    <span class="ms-2" style={{ width: '24px' }}>۳</span>
-                                                    <i class="fas fa-star text-warning ms-2"></i>
-                                                    <div class="progress flex-grow-1 ms-2"
-                                                        style={{ height: '6px', backgroundColor: '#f2f2f2' }}>
-                                                        <div class="progress-bar bg-warning" style={{ width: '8%' }}></div>
-                                                    </div>
-                                                    <span class="text-muted" style={{ width: '36px' }}>8%</span>
-                                                </div>
-                                                <div class="d-flex align-items-center mb-1">
-                                                    <span class="ms-2" style={{ width: '24px' }}>۲</span>
-                                                    <i class="fas fa-star text-warning ms-2"></i>
-                                                    <div class="progress flex-grow-1 ms-2"
-                                                        style={{ height: '6px', backgroundColor: '#f2f2f2' }}>
-                                                        <div class="progress-bar bg-warning" style={{ width: '4%' }}></div>
-                                                    </div>
-                                                    <span class="text-muted" style={{ width: '36px' }}>4%</span>
-                                                </div>
-                                                <div class="d-flex align-items-center mb-1">
-                                                    <span class="ms-2" style={{ width: '24px' }}>۱</span>
-                                                    <i class="fas fa-star text-warning ms-2"></i>
-                                                    <div class="progress flex-grow-1 ms-2"
-                                                        style={{ height: '6px', backgroundColor: '#f2f2f2' }}>
-                                                        <div class="progress-bar bg-warning" style={{ width: '3%' }}></div>
-                                                    </div>
-                                                    <span class="text-muted" style={{ width: '36px' }}>3%</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-lg-6 mb-4">
-                                        <div>
-                                            <div class="mb-3">
-                                                <span class="fw-bold">علی رضایی</span>
-                                                <span class="text-warning ms-1">
-                                                    <i class="fas fa-star"></i><i class="fas fa-star"></i><i
-                                                        class="fas fa-star"></i><i class="fas fa-star"></i><i
-                                                            class="fas fa-star-half-alt"></i>
-                                                </span>
-                                                <span class="text-muted"
-                                                    style={{ fontSize: '12px' }}
-                                                >۲ روز پیش</span>
-                                                <div class="mt-1 text-secondary"
-                                                    style={{ fontSize: '15px' }}
-                                                >
-                                                    خیلی راضی‌ام، کیفیت عالی و ارسال سریع بود.
-                                                </div>
-                                            </div>
-                                            <div class="mb-3">
-                                                <span class="fw-bold">مریم محمدی</span>
-                                                <span class="text-warning ms-1">
-                                                    <i class="fas fa-star"></i><i class="fas fa-star"></i><i
-                                                        class="fas fa-star"></i><i class="fas fa-star"></i><i
-                                                            class="far fa-star"></i>
-                                                </span>
-                                                <span class="text-muted" style={{ fontSize: '12px' }}>۱ هفته پیش</span>
-                                                <div class="mt-1 text-secondary"
-                                                    style={{ fontSize: '15px' }}
-                                                >
-                                                    نسبت به قیمتش ارزش خرید داره، اما بسته‌بندی بهتر می‌تونست باشه.
-                                                </div>
-                                            </div>
-                                            <br />
-                                            <div class="d-flex justify-content-center align-items-center">
-                                                <button class="btn-more-comment">مشاهده بیشتر</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                        <PriceHistory/>
+                        <PriceHistory />
                         <FeaturesComponent />
 
                         <section id="section5" class="Similar-products">
@@ -338,14 +141,7 @@ export default function ProductPage() {
                         </section>
 
                     </div>
-                    <aside class="col-lg-3 position-relative"
-                        style={{ marginTop: '72.5px' }}
-                    >
-                        <div class="ads">
-                            <img class="img-fluid rounded-2" src="/img/ads3.png" alt="" />
-
-                        </div>
-                    </aside>
+                    <AdsComponent />
                 </div >
             </div >
 
