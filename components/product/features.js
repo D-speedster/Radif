d639b9d27e3fbd2ffcafb817da991bd68d87d16a
D-speedster <mrspeed717@gmail.com>
2025-05-09 11:07:33 +0330

    create component features

---
+++ components/product/features.js
@@ -0,0 +1,173 @@
+export default function FeaturesComponent() {
+    return (
+        <section dir="rtl" id="section4" className="features-fa">
+            <div className="container">
+                <h3 className="mt-5">مشخصات</h3>
+                <div className="row gy-4">
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="feature-block">
+                            <div className="feature-block-title">مشخصات محصول</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>نام محصول</td>
+                                    <td>آیفون ۱۶ پرو مکس، ۲۵۶ گیگابایت</td>
+                                </tr>
+                                    <tr>
+                                        <td>برند</td>
+                                        <td>اپل</td>
+                                    </tr>
+                                </tbody></table>
+                        </div>
+                        <div className="feature-block">
+                            <div className="feature-block-title">سری</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>سری</td>
+                                    <td>آیفون ۱۶</td>
+                                </tr>
+                                </tbody></table>
+                        </div>
+                        <div className="feature-block">
+                            <div className="feature-block-title">ویژگی‌های محصول</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>رنگ‌ها</td>
+                                    <td>مشکی، بژ، سفید</td>
+                                </tr>
+                                    <tr>
+                                        <td>اندازه صفحه</td>
+                                        <td>۶.۹ اینچ</td>
+                                    </tr>
+                                    <tr>
+                                        <td>کلاس IP</td>
+                                        <td>IP68</td>
+                                    </tr>
+                                    <tr>
+                                        <td>گوشی مناسب سالمندان</td>
+                                        <td className="no">✗ خیر</td>
+                                    </tr>
+                                    <tr>
+                                        <td>جنس بدنه</td>
+                                        <td>تیتانیوم، شیشه</td>
+                                    </tr>
+                                    <tr>
+                                        <td>سال عرضه</td>
+                                        <td>۱۴۰۳</td>
+                                    </tr>
+                                    <tr>
+                                        <td>سیستم عامل</td>
+                                        <td>iOS</td>
+                                    </tr>
+                                </tbody></table>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="feature-block">
+                            <div className="feature-block-title">ویژگی‌ها</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>تا شو</td>
+                                    <td className="no">✗ خیر</td>
+                                </tr>
+                                    <tr>
+                                        <td>مقاوم در برابر ضربه</td>
+                                        <td className="no">✗ خیر</td>
+                                    </tr>
+                                    <tr>
+                                        <td>امکانات ویژه</td>
+                                        <td>سازگاری با سمعک، اسپیکر استریو</td>
+                                    </tr>
+                                    <tr>
+                                        <td>ضد آب</td>
+                                        <td className="yes">✓ بله</td>
+                                    </tr>
+                                    <tr>
+                                        <td>سنسورهای داخلی</td>
+                                        <td>بارومتر، سنسور مجاورت، ژیروسکوپ/شتاب‌سنج، اثر انگشت، تشخیص چهره</td>
+                                    </tr>
+                                </tbody></table>
+                        </div>
+                        <div className="feature-block">
+                            <div className="feature-block-title">صفحه نمایش</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>لمسی</td>
+                                    <td className="yes">✓ بله</td>
+                                </tr>
+                                    <tr>
+                                        <td>صفحه دوم</td>
+                                        <td className="no">✗ خیر</td>
+                                    </tr>
+                                    <tr>
+                                        <td>نوع صفحه</td>
+                                        <td>OLED</td>
+                                    </tr>
+                                    <tr>
+                                        <td>نرخ نوسازی</td>
+                                        <td>۱۲۰ هرتز</td>
+                                    </tr>
+                                    <tr>
+                                        <td>رزولوشن</td>
+                                        <td>۱۳۲۰×۲۸۶۶</td>
+                                    </tr>
+                                    <tr>
+                                        <td>تراکم پیکسلی</td>
+                                        <td>۴۶۰</td>
+                                    </tr>
+                                </tbody></table>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="feature-block">
+                            <div className="feature-block-title">ارتباطات</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>۴G</td>
+                                    <td className="yes">✓ بله</td>
+                                </tr>
+                                    <tr>
+                                        <td>دو سیم کارت هیبریدی</td>
+                                        <td className="no">✗ خیر</td>
+                                    </tr>
+                                    <tr>
+                                        <td>GPS</td>
+                                        <td className="yes">✓ بله</td>
+                                    </tr>
+                                    <tr>
+                                        <td>۵G</td>
+                                        <td className="yes">✓ بله</td>
+                                    </tr>
+                                    <tr>
+                                        <td>فرکانس ۵G</td>
+                                        <td>mmWave, Sub-6 GHz</td>
+                                    </tr>
+                                    <tr>
+                                        <td>سیم کارت</td>
+                                        <td>Nano-SIM, eSIM</td>
+                                    </tr>
+                                </tbody></table>
+                        </div>
+                        <div className="feature-block">
+                            <div className="feature-block-title">بی‌سیم</div>
+                            <table className="feature-table">
+                                <tbody><tr>
+                                    <td>NFC</td>
+                                    <td className="yes">✓ بله</td>
+                                </tr>
+                                    <tr>
+                                        <td>استاندارد Wi-Fi</td>
+                                        <td>Wi-Fi 7 (802.11be)</td>
+                                    </tr>
+                                    <tr>
+                                        <td>بلوتوث</td>
+                                        <td>۵.۳</td>
+                                    </tr>
+                                </tbody></table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+
+}
--- pages/product/[slug]/index.js
+++ pages/product/[slug]/index.js
@@ -6,7 +6,7 @@ import SectionProduct from '../../../components/product/sectionProduct'
 import FooterMain from '../../../components/main/FooterMain'
 import SliderProduct from '../../../components/main/SliderProduct'
 import SellersComponent from '../../../components/product/sellers'
-
+import FeaturesComponent from '../../../components/product/features'
 export default function ProductPage() {
     const router = useRouter()
     const { slug } = router.query
@@ -60,7 +60,7 @@ export default function ProductPage() {
             delivery: 'paid',
             credit: 'yes'
         },
-  
+
         {
             id: 11,
             title: 'پاوربانک Anker 20,000mAh | شارژ سریع PD | مدل A1281',
@@ -70,7 +70,7 @@ export default function ProductPage() {
             delivery: 'paid',
             credit: 'yes'
         },
-       
+
     ]
 
     return (
@@ -340,174 +340,8 @@ export default function ProductPage() {
                             </p>
                             <canvas id="myChart" width={700} height={300} />
                         </section>
-                        <section dir="rtl" id="section4" className="features-fa">
-                            <div className="container">
-                                <h3 className="mt-5">مشخصات</h3>
-                                <div className="row gy-4">
-                                    <div className="col-12 col-md-6 col-lg-4">
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">مشخصات محصول</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>نام محصول</td>
-                                                    <td>آیفون ۱۶ پرو مکس، ۲۵۶ گیگابایت</td>
-                                                </tr>
-                                                    <tr>
-                                                        <td>برند</td>
-                                                        <td>اپل</td>
-                                                    </tr>
-                                                </tbody></table>
-                                        </div>
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">سری</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>سری</td>
-                                                    <td>آیفون ۱۶</td>
-                                                </tr>
-                                                </tbody></table>
-                                        </div>
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">ویژگی‌های محصول</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>رنگ‌ها</td>
-                                                    <td>مشکی، بژ، سفید</td>
-                                                </tr>
-                                                    <tr>
-                                                        <td>اندازه صفحه</td>
-                                                        <td>۶.۹ اینچ</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>کلاس IP</td>
-                                                        <td>IP68</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>گوشی مناسب سالمندان</td>
-                                                        <td className="no">✗ خیر</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>جنس بدنه</td>
-                                                        <td>تیتانیوم، شیشه</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>سال عرضه</td>
-                                                        <td>۱۴۰۳</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>سیستم عامل</td>
-                                                        <td>iOS</td>
-                                                    </tr>
-                                                </tbody></table>
-                                        </div>
-                                    </div>
-                                    <div className="col-12 col-md-6 col-lg-4">
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">ویژگی‌ها</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>تا شو</td>
-                                                    <td className="no">✗ خیر</td>
-                                                </tr>
-                                                    <tr>
-                                                        <td>مقاوم در برابر ضربه</td>
-                                                        <td className="no">✗ خیر</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>امکانات ویژه</td>
-                                                        <td>سازگاری با سمعک، اسپیکر استریو</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>ضد آب</td>
-                                                        <td className="yes">✓ بله</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>سنسورهای داخلی</td>
-                                                        <td>بارومتر، سنسور مجاورت، ژیروسکوپ/شتاب‌سنج، اثر انگشت، تشخیص چهره</td>
-                                                    </tr>
-                                                </tbody></table>
-                                        </div>
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">صفحه نمایش</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>لمسی</td>
-                                                    <td className="yes">✓ بله</td>
-                                                </tr>
-                                                    <tr>
-                                                        <td>صفحه دوم</td>
-                                                        <td className="no">✗ خیر</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>نوع صفحه</td>
-                                                        <td>OLED</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>نرخ نوسازی</td>
-                                                        <td>۱۲۰ هرتز</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>رزولوشن</td>
-                                                        <td>۱۳۲۰×۲۸۶۶</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>تراکم پیکسلی</td>
-                                                        <td>۴۶۰</td>
-                                                    </tr>
-                                                </tbody></table>
-                                        </div>
-                                    </div>
-                                    <div className="col-12 col-md-6 col-lg-4">
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">ارتباطات</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>۴G</td>
-                                                    <td className="yes">✓ بله</td>
-                                                </tr>
-                                                    <tr>
-                                                        <td>دو سیم کارت هیبریدی</td>
-                                                        <td className="no">✗ خیر</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>GPS</td>
-                                                        <td className="yes">✓ بله</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>۵G</td>
-                                                        <td className="yes">✓ بله</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>فرکانس ۵G</td>
-                                                        <td>mmWave, Sub-6 GHz</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>سیم کارت</td>
-                                                        <td>Nano-SIM, eSIM</td>
-                                                    </tr>
-                                                </tbody></table>
-                                        </div>
-                                        <div className="feature-block">
-                                            <div className="feature-block-title">بی‌سیم</div>
-                                            <table className="feature-table">
-                                                <tbody><tr>
-                                                    <td>NFC</td>
-                                                    <td className="yes">✓ بله</td>
-                                                </tr>
-                                                    <tr>
-                                                        <td>استاندارد Wi-Fi</td>
-                                                        <td>Wi-Fi 7 (802.11be)</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>بلوتوث</td>
-                                                        <td>۵.۳</td>
-                                                    </tr>
-                                                </tbody></table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
+                        <FeaturesComponent />
+                  
                         <section id="section5" class="Similar-products">
                             <SliderProduct></SliderProduct>
                         </section>
