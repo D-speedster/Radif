5e8700d27aed145cfd3f6271324fd63817fb84ec
D-speedster <mrspeed717@gmail.com>
2025-05-16 10:15:17 +0330

    Replace Basic Icon with react-icons for Slider Like icons

--- components/main/SliderProduct.js
+++ components/main/SliderProduct.js
@@ -1,21 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
-import Link from 'next/link'; // برای Next.js
-// اگر از React Router استفاده می‌کنید، از این خط استفاده کنید
-// import { Link } from 'react-router-dom'; // برای React Router
-// Import Lucide icons - if you don't have lucide-react installed
-// use your own heart icon or install it with: npm install lucide-react
-// For now, I'll create a simple heart icon component
-const HeartIcon = () => (
-  <svg style={{ height: '18px', width: '18px', marginTop: '3.5px' }} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-  </svg>
-);
-
-// Import Swiper styles
+import Link from 'next/link';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { FaRegHeart } from "react-icons/fa";
 
 const PopularProductsSlider = () => {
   const products = [
@@ -126,7 +115,7 @@ const PopularProductsSlider = () => {
                     </div>
                     <div className="img-like">
                       <span>
-                        <HeartIcon />
+                        <FaRegHeart fontSize={'20px'} className='pt-1'/>
                       </span>
                     </div>
                   </div>
