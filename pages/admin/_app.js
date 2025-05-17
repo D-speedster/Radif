31142fde0984d2848d5090636f821385a91f9dad
d-speedster <mrspeed717@gmail.com>
2025-05-17 11:08:43 +0330

    update admin layout

--- pages/admin/_app.js
+++ pages/admin/_app.js
@@ -1,11 +1,12 @@
-
 import Script from "next/script";
+import AdminLayout from "../AdminLayout";
+
 export default function MyApp({ Component, pageProps }) {
 
   return (
-    <MainLayout>
+    <AdminLayout>
       <Component {...pageProps} />
-    </MainLayout>
+    </AdminLayout>
   );
 
 }
